886d425038fba9ef3e15b68fd3039569d8fa9df5
Taylor <wwwaaavvveeerrr@gmail.com>
2019-08-28 02:46:08 -0400

    encplicated

--- supp2u/src/App.js
+++ supp2u/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
+import Map from './comp/Map'
 import './App.css';
-import GoogleMaps from './comp/GoogleMap'
+
 
 function App() {
   return (
-    <div className="App">
-
-        
+    <div className="App"> 
         <p>
-          Welcome to 
+          {/* Welcome to Supp2U */}
         </p>
-        <h1>Supp2U</h1>
-
-        <GoogleMaps
-        isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyA9vZYucirjbKCttIzPuBEbNelsYeCsAYg&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}/>
-        
-      
+        <Map className="map" />
     </div>
   );
 }
--- supp2u/src/comp/GoogleMap.js
+++ supp2u/src/comp/GoogleMap.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { GoogleMap, Marker, withGoogleMap,withScriptjs } from "react-google-maps"
 
+
 const GoogleMaps = withScriptjs(withGoogleMap((props) =>
+
   <GoogleMap
+  className="map"
     defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultCenter={{ lat: 23.118813, lng: -82.329933  }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: 23.118813, lng: -82.329933 }} />}
   </GoogleMap>))
 
 
