53ac26deafa712029bd3b4d5a8c54528dfc7ef02
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-08-29 09:38:34 -0700

    business view setup

---
+++ supp2u/src/comp/Business/BusinessList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import Character from "./Character";
+import Business from "./Business";
+
+const BusinessList = props => {
+  return (
+    <ul>
+      {props.businesses.map(business => {
+        return <Business key={business.name} business={business} />;
+      })}
+    </ul>
+  );
+};
+
+export default BusinessList;
---
+++ supp2u/src/comp/Business/BusinessListView.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { connect } from "react-redux";
+
+// import { CharacterList } from "../components";
+// import { getCharacters } from "../actions";
+
+class BusinessListView extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.props.getBusinesses();
+  }
+
+  render() {
+    if (this.props.fetching) {
+      <h2>Fetching business data...</h2>;
+    }
+    return (
+      <div>
+        <BusinessList businesses={this.props.businesses} />;
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    businesses: state.businsReducer.businesses,
+    error: state.businsReducer.error,
+    fetching: state.businsReducer.fetching
+  };
+};
+
+// export default connect(
+//   mapStateToProps,
+//   { getBusinesses }
+// )(BusinessListView);
