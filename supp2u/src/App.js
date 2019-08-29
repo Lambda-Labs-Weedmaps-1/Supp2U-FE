02379924bba3f8582a035acc77f157be2d0b247e
Henry Neal <henry.neal0@gmail.com>
2019-08-29 11:53:22 -0400

    fixing netlify deploy

--- supp2u/src/App.js
+++ supp2u/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from './comp/googleMap/Map'
 import Navigation from './comp/Navigation/Navigation'
 import './App.css';
+<<<<<<< HEAD
 import LookupTest from './comp/LookupTest';
 import BusinessAdd from './comp/Business/BusinessAdd';
 
@@ -23,6 +24,27 @@ function App() {
         </div>
     </div>
   );
+=======
+import GoogleMaps from './comp/GoogleMap';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Welcome to</p>
+				<h1>Supp2U</h1>
+
+				<GoogleMaps
+					isMarkerShown
+					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyA9vZYucirjbKCttIzPuBEbNelsYeCsAYg&v=3.exp&libraries=geometry,drawing,places`}
+					loadingElement={<div style={{ height: `100%` }} />}
+					containerElement={<div style={{ height: `400px` }} />}
+					mapElement={<div style={{ height: `100%` }} />}
+				/>
+			</header>
+		</div>
+	);
+>>>>>>> 3343566fe8d6a217a9b1b9e3d4343c46eb56703f
 }
 
-export default App;
+// export default App;
--- supp2u/src/index.js
+++ supp2u/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+<<<<<<< HEAD
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+=======
+// import App from './App';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+>>>>>>> 3343566fe8d6a217a9b1b9e3d4343c46eb56703f
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
