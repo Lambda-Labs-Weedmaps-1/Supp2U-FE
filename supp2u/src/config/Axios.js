5b0d894685eb1eebef0fa086d5241bef480418d5
sibhat <sibhat.temelso@gmail.com>
2019-09-02 16:46:43 -0700

    🎯 implement axios instance to set Authorization header if token is available on localstorage.

--- supp2u/src/config/Axios.js
+++ supp2u/src/config/Axios.js
@@ -1,4 +1,9 @@
 import axios from "axios";
-export default axios.create({
+const instance = axios.create({
     baseURL: `${process.env.REACT_APP_API_BACKEND_URL}/`
 });
+if(localStorage.getItem("token_id")){
+    instance.defaults.headers.common['Authorization'] = localStorage.getItem("id_token");
+
+}
+export {instance};
