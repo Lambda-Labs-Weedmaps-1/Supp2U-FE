e06fe33fc5dc0a3d0757d8daa512f5f228a20252
Julian Kohlman <juliankohlman@gmail.com>
2019-09-02 22:17:09 -0400

    Auth0 tokenID update > Co-authored-by: Sibhat sibhat.temelso@gmail.com

--- supp2u/src/utils/init.js
+++ supp2u/src/utils/init.js
@@ -6,7 +6,7 @@ export const auth = new AuthService(
 	'mCXBPD5PbbLbRS8sKVKm6Y5Pb5c3wIQB',
 	'supp2u.auth0.com'
 );
-``
+
 export const api = axios.create({
   //! change to whatever port you're running front end from //
 	baseURL: 'http://localhost:3001'
