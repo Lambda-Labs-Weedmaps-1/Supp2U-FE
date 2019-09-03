b31e475aaa99f5dd30c8397863803a98ca29247c
Taylor <wwwaaavvveeerrr@gmail.com>
2019-09-03 14:56:44 -0400

    ratings now display

--- supp2u/src/components/Business/BusinessSingleView.js
+++ supp2u/src/components/Business/BusinessSingleView.js
@@ -8,8 +8,11 @@ function BusinessSingleView() {
 
     // this sets the state to the information of the business called        
     const [info, setInfo] = useState([{}])
+    // this sets the rating of the business
+    const [rating, setRating] = useState({ "data":"loading..." })
 
     useEffect(() => {
+        // api GET to bring in all the info for the business
         Axios.get(`${process.env.REACT_APP_BACKEND_URL}businesses/1`)
         .then(res =>{
             console.log('bus info incoming', res.data);
@@ -18,8 +21,17 @@ function BusinessSingleView() {
         .catch(err =>{
             console.log('ERROR POST\n', err)
         })
-       
+        //api GET to bring in the rating
+        Axios.get(`${process.env.REACT_APP_BACKEND_URL}businesses/1/ratings`)
+        .then(res =>{
+            console.log('rating incoming', res.data);
+            setRating(res)
+        })
+        .catch(err =>{
+            console.log('ERROR POST\n', err)
+        })
     }, [])
+
 
     return (
     <>
@@ -27,6 +39,9 @@ function BusinessSingleView() {
         <h2>{info.name}</h2>
         <div>
         {info.street}, {info.city}, {info.zipcode}, {info.state}
+        </div>
+        <div>
+        Rating: {rating.data}
         </div>
     </div>
     <MenuShowcase />
--- supp2u/src/components/Reviews/list/ReviewPresentation.js
+++ supp2u/src/components/Reviews/list/ReviewPresentation.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
-export default (props)=>{
+import './reviews.sass'
+
+function ReviewPresentation() {
+
+    const [review, setReview] = useState([{
+        
+    }])
+
+    useEffect(() => {
+        Axios.get(`${process.env.REACT_APP_BACKEND_URL}businesses/:business_id/reviews`)
+        .then(res=>{
+            setReview(res.data)
+        })
+        .catch(err=>{
+            console.log('ERROR POST\n', err)
+        })
+    }, [])
+
     return (
         <div>
-        Placeholder
-    </div>)
+            {/* write a codntial render here */}
+        </div>
+    )
 }
+
+export default ReviewPresentation
