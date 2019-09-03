0999c142725f46640537af4f806d2a0b0ed2c84e
amberm <nekosenpai@gmail.com>
2019-09-03 13:51:48 -0600

    initial landing page for registration

---
+++ supp2u/src/components/InitLandingPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { BusinessList } from "../components/Business/BusinessList"
+import { getBusinessInfo } from "../actions/getBusiness"
+
+class InitLandingPage extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    
+  }
+
+  goCust = () => {
+      window.location.href ='/customer/create'
+  }
+
+  goBus = () => {
+    window.location.href ='/business/create'
+}
+
+  render() {
+    // if (this.props.gettingBusinesses) {
+    //   <h2>Fetching USER data...</h2>;
+    // }
+    return (
+      <div>
+        <h2>Register as a Customer</h2>
+        <button onClick={this.goCust}>Customer</button>
+        <br></br><hr></hr><br></br>
+        <h2>Register as a Business</h2>
+        <button onClick={this.goBus}>Business</button>
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = state => {
+//   return {  
+//   };
+// };
+
+export default connect(
+//   mapStateToProps,
+//   { }
+)(InitLandingPage);
