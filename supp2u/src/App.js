ad8670162d26dda76e4eb2d5c6046aedb4396998
Julian Kohlman <juliankohlman@gmail.com>
2019-09-03 19:47:40 -0400

    business list view useless constructor removed

--- supp2u/src/App.js
+++ supp2u/src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import Map from './components/googleMap/Map';
-import {Route} from "react-router-dom";
+// import { Route } from 'react-router-dom';
 import './App.sass';
-import BasicRoute from "./BasicRoute";
+import BasicRoute from './BasicRoute';
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 toast.configure();
 
-
 function App() {
-  return (
-    <div className="App">
-
-
-        <BasicRoute />
-        <div className="Map_Holder">
-          <Map />
-        </div>
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<BasicRoute />
+			<div className="Map_Holder">
+				<Map />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- supp2u/src/views/BusinessListView.js
+++ supp2u/src/views/BusinessListView.js
@@ -1,40 +1,36 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { BusinessList } from "../components/Business/BusinessList"
-import { getBusinessInfo } from "../actions/getBusiness"
+import { BusinessList } from '../components/Business/BusinessList';
+import { getBusinessInfo } from '../actions/getBusiness';
 
 class BusinessListView extends React.Component {
-  constructor() {
-    super();
-  }
+	componentDidMount() {
+		this.props.getBusinessInfo();
+	}
 
-  componentDidMount() {
-    this.props.getBusinessInfo();
-  }
-
-  render() {
-    // if (this.props.gettingBusinesses) {
-    //   <h2>Fetching business data...</h2>;
-    // }
-    return (
-      <div>
-        <BusinessList businesses={this.props.businesses} />;
-      </div>
-    );
-  }
+	render() {
+		// if (this.props.gettingBusinesses) {
+		//   <h2>Fetching business data...</h2>;
+		// }
+		return (
+			<div>
+				<BusinessList businesses={this.props.businesses} />;
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  console.log('businessListView state',state);
-  return {
-    businesses: state.businessGet.businesses,
-    error: state.businessGet.error,
-    gettingBusinesses: state.businessGet.gettingBusinesses
-  };
+	console.log('businessListView state', state);
+	return {
+		businesses: state.businessGet.businesses,
+		error: state.businessGet.error,
+		gettingBusinesses: state.businessGet.gettingBusinesses
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  { getBusinessInfo }
+	mapStateToProps,
+	{ getBusinessInfo }
 )(BusinessListView);
