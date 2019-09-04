35d8c0254b9f5696f32ef75b8f43ef5781eb0ec8
sibhat <sibhat.temelso@gmail.com>
2019-09-04 14:37:45 -0700

    implement edit functionality to reviews.

--- supp2u/src/components/Reviews/add/index.js
+++ supp2u/src/components/Reviews/add/index.js
@@ -1,25 +1,40 @@
 import React from "react";
 import {addReview} from "../../../actions/reviewsAction";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import AddReviewPresentation from "./AddReviewPresentation";
 import {toast} from "react-toastify";
+import EditReveiwPresentaion from "../edit/EditReviewPresentation";
 
-const mapPropsToState = ({customerReducer: {customer}}) =>({customer});
-export default connect(mapPropsToState, {addReview})(({addReview, history, customer, business_id})=>{
+const mapPropsToState = ({customerReducer: {customer}}) => ({customer});
+
+// edit={true} review={review}
+
+export default connect(mapPropsToState, {addReview})(({addReview, history, customer, business_id, edit, review}) => {
 
 
-    const addReviewHandler =(review) =>{
-        
+    const addReviewHandler = (review) => {
+
         addReview({...review, business_id}, localStorage.customer_id)
             .then((res) => {
-                if(res.review){
+                if (res.review) {
                     toast.success(`Thank you for your feedback ${res.review}`);
                     // history.goBack();
-                }else{
+                } else {
                     toast.error(`🔐 ️${res.response.data.message}`);
                     console.log();
                 }
             });
     };
-    return <AddReviewPresentation addReview={addReviewHandler} history={history}/>
+    const editReviewHandler= (review) => {
+        // patch axios calll to edit a review.
+
+    };
+    return (
+        <>
+            {
+                edit ? <EditReveiwPresentaion editReview={editReviewHandler}  review={review}/> :
+                    <AddReviewPresentation addReview={addReviewHandler} history={history}/>
+            }
+        </>
+    )
 })
