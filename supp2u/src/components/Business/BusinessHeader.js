4444c34ac39f3ab611781cbbe5793d9a01be73c2
Taylor <wwwaaavvveeerrr@gmail.com>
2019-09-06 03:54:59 -0400

    when seeting hours of op when chosing to leave a input blank it will defualt to CLOSED

--- supp2u/src/components/Business/BusinessHeader.js
+++ supp2u/src/components/Business/BusinessHeader.js
@@ -4,7 +4,7 @@ import './businessSingleView.sass'
 
 function BusinessHeader(props) {
 
-    console.log(props.info)
+    console.log(props.info[2].saturday, 'hip')
 
     return (
       <div className="business-header">
@@ -16,16 +16,18 @@ function BusinessHeader(props) {
         <div>
         Rating: {props.info[1].data}
         </div>
+        {props.info[2] === null ? 
+        <p>This business does not have any Hours of Operations set</p> :
         <div>
           <h4>Hours of Operation</h4>
-        <p>Sunday {props.info[2].sunday}</p>
-        <p>Monday {props.info[2].monday}</p>
-        <p>Tuesday {props.info[2].tuesday}</p>
-        <p>Wednesday {props.info[2].wednesday}</p>
-        <p>Thursday {props.info[2].thursday}</p>
-        <p>Friday {props.info[2].friday}</p>
-        <p>Saturday {props.info[2].saturday}</p>
-        </div>
+        {props.info[2].sunday === null ? <p>Sunday: Closed</p>:  <p>Sunday: {props.info[2].sunday}</p>}
+        {props.info[2].monday === null ? <p>Monday: Closed</p>:  <p>Monday: {props.info[2].monday}</p>}
+        {props.info[2].tuesday === null ? <p>Tuesday: Closed</p>:  <p>Tuesday: {props.info[2].tuesday}</p>}
+        {props.info[2].wednesday === null ? <p>Wednesday: Closed</p>:  <p>Wednesday: {props.info[2].wednesday}</p>}
+        {props.info[2].thursday === null ? <p>Thursday: Closed</p>:  <p>Thursday: {props.info[2].thursday}</p>}
+        {props.info[2].friday === null ? <p>Friday: Closed</p>:  <p>Friday: {props.info[2].friday}</p>}
+        {props.info[2].saturday === null ? <p>Saturday: Closed</p>:  <p>Saturday: {props.info[2].saturday}</p>}
+        </div>}
     </div>
     )
 }
