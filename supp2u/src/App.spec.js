5fb68f58a780999abbc02b14ed7705133935d381
Taylor <wwwaaavvveeerrr@gmail.com>
2019-09-09 12:11:59 -0400

    wrote a simple single test for App.js to set up a test that of failed everything fails

---
+++ supp2u/src/App.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+// this is an extremely simple est to just see if the app is rendering at all, if this fails all is lost
+describe('APP', ()=>{
+    it ('Renders without crashing', ()=>{
+        const application = shallow(<App />);
+    
+      expect(application).toMatchSnapshot();
+    })
+
+})
