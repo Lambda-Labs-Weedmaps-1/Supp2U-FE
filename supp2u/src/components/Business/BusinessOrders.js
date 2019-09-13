67a841782fb90f08558dd41e6a4171ff3ab2989a
amberm <nekosenpai@gmail.com>
2019-09-13 08:47:39 -0600

    updated orders view to bring in info from BE

--- supp2u/src/components/Business/BusinessOrders.js
+++ supp2u/src/components/Business/BusinessOrders.js
@@ -7,9 +7,10 @@ import './businessSingleView.sass'
 
 function BusinessOrders(props) {
     
-    let businy = props.match.params.id;
+    let businy = localStorage.getItem("business_id")
    
     const [info, setInfo] = useState([{}])
+    const [orders, setOrders] = useState({})
     
     
 
@@ -22,7 +23,17 @@ function BusinessOrders(props) {
         .catch(err =>{
             console.log('ERROR GETTING BUSINESS\n', err)
         })
-        
+
+        Axios.get(`${process.env.REACT_APP_BACKEND_URL}businesses/1/orders`)
+        .then(res => {
+            setOrders(res.data)
+            console.log(res.data)
+            
+
+        })
+        .catch(err => {
+            console.log('Error getting business ORDERS ', err)
+        })
         
     }, [])
 
@@ -35,6 +46,35 @@ function BusinessOrders(props) {
         <div>
             <h3>{info.name} Orders</h3>
             <p>A placeholder obviously.</p>
+            { orders[0] == undefined ? <p>Loading the Orders...</p> :
+            <div>
+            <h3>Orders are here</h3>
+            <div>
+                {orders.map(order => {
+                    return (
+                        <div>
+                            <p>----------------------------------------------------</p>
+                            <p>Order ID: {order.id}</p>
+                            <p>Order Customer: {order.customer_id}</p>
+                            <p>Order Items:</p>
+                            <p>_____________</p>
+                            {order.items.map(inneritems => {
+                                return(
+                                    <div>
+                                       <p>{inneritems.id} - {inneritems.name} - {inneritems.price}</p>
+                                    </div>
+                            
+                                )
+                            })}
+                            <p>_____________</p>
+                            <p>Order Status: {order.status}</p>
+                            <p>----------------------------------------------------</p>
+                        </div>
+                    )
+                })}
+            </div>
+            </div>
+            }
         </div>
     
     )
