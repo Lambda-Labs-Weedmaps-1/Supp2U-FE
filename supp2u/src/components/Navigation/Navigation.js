b59c343412ba87e20a4fa3a3b192fbc85b89ba53
Henry Neal <henry.neal0@gmail.com>
2019-09-16 13:52:56 -0400

    main nav and landing page styled

--- supp2u/src/components/Navigation/Navigation.js
+++ supp2u/src/components/Navigation/Navigation.js
@@ -6,6 +6,7 @@ import BusinessNav from "./BusinessNav";
 import CustomerNav from "./CustomerNav";
 import { withRouter } from "react-router";
 import "./nav.sass";
+import Image from "../../images/Image5.png"
 
 function Navigation(props) {
   let renderLogInLogOut = () => {
@@ -15,13 +16,13 @@ function Navigation(props) {
       localStorage.getItem("user_id")
     ) {
       return (
-        <Button color="primary" onClick={auth.logout}>
+        <Button className="auth-button"  onClick={auth.logout}>
           Sign out
         </Button>
       );
     } else {
       return (
-        <Button color="primary" onClick={() => auth.login(props.history)}>
+        <Button className="auth-button"  onClick={() => auth.login(props.history)}>
           Sign In / Sign Up
         </Button>
       );
@@ -47,12 +48,15 @@ function Navigation(props) {
   return (
     <div>
       <nav className="Navigation">
-        <h1 className="NavTitle">supp2u</h1>
-        <Link className="Link" to="/">
+        <img src={Image} alt="logo" width="15%" height="130%" />
+        {/* <Link className="Link" to="/">
           Home
-        </Link>
+        </Link> */}
         {renderUserType()}
-        {renderLogInLogOut()}
+        <div>
+          {renderLogInLogOut()}
+        </div>
+        
       </nav>
     </div>
   );
